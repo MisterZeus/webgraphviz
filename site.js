e98c316603132fb713bcc0598a69ff762dfc519d
Orestes Zoupanos <orestes.zoupanos@tpr.gov.uk>
2026-03-13 10:40:35 +0000

    update jquery

--- site.js
+++ site.js
@@ -4,12 +4,12 @@ jQuery(document).ready(function () {
 
   // Installs error handling.
   jQuery.ajaxSetup({
-  error: function(resp, e) {
-    if (resp.status == 0){
-      alert('You are offline!!\n Please Check Your Network.');
-      } else if (resp.status == 404){
+    error: function (resp, e) {
+      if (resp.status == 0) {
+        alert('You are offline!!\n Please Check Your Network.');
+      } else if (resp.status == 404) {
         alert('Requested URL not found.');
-      } else if (resp.status == 500){
+      } else if (resp.status == 500) {
         alert('Internel Server Error:\n\t' + resp.responseText);
       } else if (e == 'parsererror') {
         alert('Error.\nParsing JSON Request failed.');
@@ -20,7 +20,6 @@ jQuery(document).ready(function () {
       }
     }
   });  // error:function()
-
 
   var generate_btn = jQuery('#generate_btn');
   var sample_1_btn = jQuery('#sample_1_btn');
@@ -36,21 +35,20 @@ jQuery(document).ready(function () {
     graphviz_data_textarea.val(text);
   }
 
-
   function UpdateGraphviz() {
-	svg_div.html("");
+    svg_div.html("");
     var data = graphviz_data_textarea.val();
     // Generate the Visualization of the Graph into "svg".
     var svg = Viz(data, "svg");
-    svg_div.html("<hr>"+svg);
+    svg_div.html("<hr>" + svg);
   }
 
   // Startup function: call UpdateGraphviz
-  jQuery(function() {
-	// The buttons are disabled, enable them now that this script
-	// has loaded.
+  jQuery(function () {
+    // The buttons are disabled, enable them now that this script
+    // has loaded.
     generate_btn.removeAttr("disabled")
-                .text("Generate Graph!");
+      .text("Generate Graph!");
 
     sample_1_btn.removeAttr("disabled");
     sample_2_btn.removeAttr("disabled");
@@ -62,24 +60,23 @@ jQuery(document).ready(function () {
   // Bind actions to form buttons.
   generate_btn.click(UpdateGraphviz);
 
-  sample_1_btn.click(function(){
+  sample_1_btn.click(function () {
     InsertGraphvizText(jQuery("#sample1_text").html().trim());
   });
 
-  sample_2_btn.click(function(){
+  sample_2_btn.click(function () {
     InsertGraphvizText(jQuery("#sample2_text").html().trim());
   });
 
-  sample_3_btn.click(function(){
+  sample_3_btn.click(function () {
     InsertGraphvizText(jQuery("#sample3_text").html().trim());
   });
 
-  sample_4_btn.click(function(){
+  sample_4_btn.click(function () {
     InsertGraphvizText(jQuery("#sample4_text").html().trim());
   });
 
-  sample_5_btn.click(function(){
+  sample_5_btn.click(function () {
     InsertGraphvizText(jQuery("#sample5_text").html().trim());
   });
-
 });
